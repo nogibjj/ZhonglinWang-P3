29c5146ce3f7c2acc10038b5fb4096aab1950b75
zw277 <112585430+zw277@users.noreply.github.com>
2022-11-07 00:10:12 +0000

    update

--- sql.py
+++ sql.py
@@ -7,7 +7,7 @@ connects = sqlite3.connect("superstore.db")
 cursor = connects.cursor()
 
 ## Step 2: Create a new table called superstore (new_table)
-new_table = """CREATE TABLE superstore111(
+new_table = """CREATE TABLE superstore222(
  				unique_id VAR PRIMARY KEY,
                 Ship_Mode VAR,
                 Segment VAR,
@@ -26,7 +26,7 @@ new_table = """CREATE TABLE superstore111(
  				"""
 ## Step 3: Insert the new table into the original database, then insert data into table superstore by using SQL query
 cursor.execute(new_table)
-insert_data = "INSERT INTO superstore111 (unique_id, Ship_Mode, Segment, Country, City, State, Postal_Code, Region, Category, Sub_Category, Sales, Quantity, Discount, Profit) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+insert_data = "INSERT INTO superstore222 (unique_id, Ship_Mode, Segment, Country, City, State, Postal_Code, Region, Category, Sub_Category, Sales, Quantity, Discount, Profit) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
 ## Step 4: Read each record from superstore.csv file and import the data to new table
 file = open("superstore.csv")
@@ -35,7 +35,7 @@ cursor.executemany(insert_data, records)
 
 ### Query 1: Find the top ten states who has the most consumer customers in superstore
 query1 = """SELECT Distinct State, count(Segment) 
-		   FROM superstore111
+		   FROM superstore222
 		   WHERE State != '' AND Segment='Consumer'
 		   Group by State
 		   Order by count(Segment) DESC
@@ -44,7 +44,7 @@ top10_consumer = cursor.execute(query1).fetchall()
 
 ### Query 2: Find the top ten states who has the most corporate customers in superstore
 query2 = """SELECT Distinct State, count(Segment) 
-		   FROM superstore111
+		   FROM superstore222
 		   WHERE State != '' AND Segment='Corporate'
 		   Group by State
 		   Order by count(Segment) DESC
@@ -53,7 +53,7 @@ top10_corporate = cursor.execute(query2).fetchall()
 
 ### Query 3: Find the top ten cities who has the most profit in superstore
 query3 = """SELECT Distinct City, sum(Profit) 
-		   FROM superstore111
+		   FROM superstore222
 		   WHERE City != '' 
 		   Group by City
 		   Order by count(Profit) DESC
